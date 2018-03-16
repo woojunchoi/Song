c8c255589aa59294bc8c748c78c390d83cf68a9d
woojunchoi <tim.woojune1@gmail.com>
2018-03-16 01:29:42 -0700

    basic setup finished

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM, {render} from 'react-dom';
+
+const Root = () => {
+  return <div>Lyrical</div>
+};
+
+render(
+  <Root />,
+  document.querySelector('#root')
+);
